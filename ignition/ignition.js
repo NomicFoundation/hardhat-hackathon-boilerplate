dd24572dd3da6b4bb32a039c1b769007bcd028bc
Travis Jerome Goff <99358631+VersoriumX@users.noreply.github.com>
2025-01-01 18:53:26 -0500

    Update ignition.js

--- ignition/ignition.js
+++ ignition/ignition.js
@@ -5,7 +5,7 @@ const ONE_GWEI = 1_000_000_000_000_000n;
 
 module.exports = buildModule("LockModule", (m) => {
   const unlockTime = m.getParameter("unlockTime", JAN_1ST_2025);
-  const lockedAmount = m.getParameter("lockedAmount", ONE_GWEI);
+  const lockedAmount = m.getParameter("lockedAmount", ONEhundredthousand_GWEI);
 
   const lock = m.contract("Lock", [unlockTime], {
     value: lockedAmount,
