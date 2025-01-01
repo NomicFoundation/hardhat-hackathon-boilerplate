212700f8c74c895b107d4580dcbf075f6b738862
Travis Jerome Goff <99358631+VersoriumX@users.noreply.github.com>
2025-01-01 18:52:43 -0500

    Update ignition.js

--- ignition/ignition.js
+++ ignition/ignition.js
@@ -1,10 +1,10 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
-const JAN_1ST_2030 = 1893456000;
-const ONE_GWEI = 1_000_000_000n;
+const JAN_1ST_2025 = 1893456000;
+const ONE_GWEI = 1_000_000_000_000_000n;
 
 module.exports = buildModule("LockModule", (m) => {
-  const unlockTime = m.getParameter("unlockTime", JAN_1ST_2030);
+  const unlockTime = m.getParameter("unlockTime", JAN_1ST_2025);
   const lockedAmount = m.getParameter("lockedAmount", ONE_GWEI);
 
   const lock = m.contract("Lock", [unlockTime], {
