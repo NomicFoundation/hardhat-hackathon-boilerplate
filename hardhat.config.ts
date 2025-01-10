91bf91db59a70135ed644f9ade30869aeea10c86
Travis Jerome Goff <99358631+VersoriumX@users.noreply.github.com>
2025-01-10 07:28:58 -0500

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,7 +8,7 @@ import type { HardhatUserConfig } from 'hardhat/types'
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: '0.8.9',
+    version: '0.8.20',
   },
   paths: {
     sources: './test/contracts',
